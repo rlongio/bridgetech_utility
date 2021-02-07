c0cf7029775c3ec2673dab020183d6208702873b
Ryan Long <ryan@rlong.io>
2021-02-07 14:10:16 -0500

    First review

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 # A sample Python project
 
+"""
+Attached a CSV of an elevator operation.
+There are two types of data - button_call and door_open.
+We want to analyze the time it took to get to a floor from the time it was called.
+
+A few rules:
+
+1. The button_call can be positive or negative. That just means the person wanted to go up or down. We can treat it for now as the absolute value. So -3=3
+2. The button_call can be pushed a few times before the open_door at that floor. We only care about the first time it was pressed until the open_door.
+3. We can assume that if a call was 'open' (=meaning the button_call was sent, but the open_door at that floor didn't occur yet) for more than 10 minutes - then this is an anomaly and we do not want to count that button_call.
+
+The output should be per-day the Average and Median wait times for the elevator
+"""
+
+"""
+
+- get list of values
+- sort by date
+
+iterate:
+
+if type is button_call:
+results[floor]
+"""
+
 ![Python Logo](https://www.python.org/static/community_logos/python-logo.png "Sample inline image")
 
 A sample project that exists as an aid to the [Python Packaging User
@@ -16,13 +41,12 @@ Most of the configuration for a Python project is done in the `setup.py` file,
 an example of which is included in this project. You should edit this file
 accordingly to adapt this sample project to your needs.
 
-----
+---
 
 This is the README file for the project.
 
 The file should use UTF-8 encoding and can be written using
-[reStructuredText][rst] or [markdown][md use] with the appropriate [key set][md
-use]. It will be used to generate the project webpage on PyPI and will be
+[reStructuredText][rst] or [markdown][md use] with the appropriate [key set][md use]. It will be used to generate the project webpage on PyPI and will be
 displayed as the project homepage on common code-hosting services, and should be
 written for that purpose.
 
--- src/sample/simple.py
+++ src/sample/simple.py
@@ -1,43 +1,21 @@
 import csv
 import operator
-import os
 import statistics
 import datetime
 import typing
 import bisect
 import collections
 
-"""
-Attached a CSV of an elevator operation.
-There are two types of data - button_call and door_open.
-We want to analyze the time it took to get to a floor from the time it was called.
 
-A few rules:
-1) The button_call can be positive or negative. That just means the person wanted to go up or down. We can treat it for now as the absolute value. So -3=3
-2) The button_call can be pushed a few times before the open_door at that floor. We only care about the first time it was pressed until the open_door.
-3) We can assume that if a call was 'open' (=meaning the button_call was sent, but the open_door at that floor didn't occur yet) for more than 10 minutes - then this is an anomaly and we do not want to count that button_call.
-
-
-The output should be per-day the Average and Median wait times for the elevator
-"""
-
-"""
-* get list of values
-* sort by date
-
-iterate:
-
-if type is button_call:
-    results[floor]
-"""
-
-
+# elevator operation record
 ElevatorOperationRecord = collections.namedtuple(
     "ElevatorOpeartionRecord", ["start", "other", "end"]
 )
 
 
 class ElevatorOperation:
+    """Represents an elevator operation"""
+
     def __init__(self, record: ElevatorOperationRecord):
         self._record = record
 
@@ -45,32 +23,29 @@ class ElevatorOperation:
         return f"<{self.__class__.__name__}: {vars(self)}>"
 
     def is_anamoly(self) -> bool:
-        if self._record.start is None or self._record.end is None:
-            return True
-        if self.button_to_open() > 60 * 10:
-            return True
-        if self.button_to_open() < 0:
-            return True
-        return False
-
-    def button_to_open(self) -> int:
-        """button_to_open returns the length in seconds between the time
-        the button was called to the time the door was opened.
-
-        Returns:
-            int: [description]
-        """
-        if self._record.start is None or self._record.end is None:
-            return -1
-        return int((self._record.end.date - self._record.start.date).total_seconds())
-
-    def direction(self):
-        """direction return True for up and False for down
+        """is_anamoly returns true if anamoly is detected in the operation
 
         Returns:
             bool:
         """
-        return bool(self._record.start.floor)
+        if self._record.start is None or self._record.end is None:
+            return True
+        if self.operation_time() > 60 * 10:
+            return True
+        if self.operation_time() < 0:
+            return True
+        return False
+
+    def operation_time(self) -> int:
+        """operation_time returns the length in seconds between the time the
+        button was called to the time the door was opened.
+
+        Returns:
+            int: length of time in seconds
+        """
+        if self._record.start is None or self._record.end is None:
+            return -1
+        return int((self._record.end.date - self._record.start.date).total_seconds())
 
 
 class ElevatorOperations:
@@ -83,29 +58,41 @@ class ElevatorOperations:
     def __iter__(self):
         return iter(self._operations)
 
-    def average(self):
+    def average(self) -> float:
+        """average returns the average time to complete all contained
+        operations.
+
+        Returns:
+            float: average
+        """
         try:
             return statistics.mean(
-                x.button_to_open() for x in self._operations if not x.is_anamoly()
+                x.operation_time() for x in self._operations if not x.is_anamoly()
             )
-        except Exception as e:
+        except Exception:
             return -1
 
-    def median(self):
+    def median(self) -> float:
+        """median returns the media time to complete all contained operations.
+
+        Returns:
+            float: median
+        """
         try:
             return statistics.median(
-                x.button_to_open() for x in self._operations if not x.is_anamoly()
+                x.operation_time() for x in self._operations if not x.is_anamoly()
             )
-        except Exception as e:
+        except Exception:
             return -1
-
-    def print_report(self):
-        for item in self._operations:
-            print(item._record)
-            print(item.button_to_open())
 
     @staticmethod
     def from_log_entries(entries: "ElevatorLogEntries") -> "ElevatorOperations":
+        """from_log_entries is a convenience method for creating an
+        Elevator Operations object from ElevatorLogEntries.
+
+        Returns:
+            ElevatorLogEntries: entries
+        """
         dates: list[ElevatorLogEntries] = [
             entry for entry in entries.split_by_date().values()
         ]
@@ -131,6 +118,8 @@ class ElevatorOperations:
 
 
 class ElevatorLogEntry:
+    """ElevatorLogEntry models a log entry"""
+
     def __init__(self, id, device_id, data, type, date):
         self.id = id
         self.device_id = device_id
@@ -144,6 +133,8 @@ class ElevatorLogEntry:
 
 
 class ElevatorLogEntries:
+    """ElevatorLogEntries is a container for ElevatorLogEntry"""
+
     def __init__(self, list: typing.List["ElevatorLogEntry"]):
         self._list = sorted(list, key=lambda x: x.date)
 
@@ -153,70 +144,123 @@ class ElevatorLogEntries:
     def __iter__(self):
         return self._list.__iter__()
 
-    def append(self, entry: "ElevatorLogEntry"):
+    def insert(self, entry: "ElevatorLogEntry"):
+        """insert adds the entry to the container ordered
+
+        Args:
+            entry (ElevatorLogEntry): elevator log entry
+        """
         self._list.insert(
             bisect.bisect_left(self._list, self._list.append(entry)), entry
         )
 
     def dates(self) -> set[datetime.date]:
+        """dates returns a set of dates for each log entry in the container
+
+        Returns:
+            set[datetime.date]: set of datetime.date's
+        """
         return {x.date.date() for x in self._list}
 
     def datetimes(self) -> set[datetime.datetime]:
+        """datetimes returns a set of datetimes for each log entry in the container
+
+        Returns:
+            set[datetime.datetime]: set of datetime.datetime's
+        """
         return {x.date for x in self._list}
 
     def floors(self) -> set[int]:
+        """floors returns a set of floors for each log entry in the container
+
+        Returns:
+            set[int]: set of ints
+        """
         return {x.floor for x in self._list}
 
     def filter_date(self, _date: datetime.date) -> "ElevatorLogEntries":
+        """filter_date returns a new instance of ElevatorLogEntries that
+        match the _date parameter.
+
+        Returns:
+            ElevatorLogEntries: entries
+        """
         return ElevatorLogEntries([x for x in self._list if x.date.date() == _date])
 
     def filter_floor(self, floor: int) -> "ElevatorLogEntries":
+        """filter_floor returns a new instance of ElevatorLogEntries that
+        match the floor parameters.
+
+        Returns:
+            ElevatorLogEntries: entries
+        """
         return ElevatorLogEntries(
             [x for x in self._list if abs(int(x.floor)) == int(floor)]
         )
 
     def split_by_date(self) -> dict[datetime.date, "ElevatorLogEntries"]:
+        """split_by_date returns a dictionary with keys as unique dates
+        and value as ElevatorLogEntries corresponding to the date key.
+
+        Returns:
+            ElevatorLogEntries: entries
+        """
         results: dict[datetime.date, ElevatorLogEntries] = {}
         for _date in self.dates():
             results[_date] = self.filter_date(_date)
         return results
 
     def split_by_floor(self) -> dict[int, "ElevatorLogEntries"]:
+        """split_by_floor returns a dictionary with keys as unique floors
+        and value as ElevatorLogEntries corresponding to the floor key.
+
+        Returns:
+            ElevatorLogEntries: entries
+        """
         results: dict[int, ElevatorLogEntries] = {}
         for floor in self.floors():
             results[floor] = self.filter_floor(floor)
         return results
 
 
-class ElevatorCall:
-    def __init__(self, entry: "ElevatorLogEntry"):
-        self.floor: int = entry.floor
-        self.button_call: datetime.datetime = entry.date
-        self.open_door: typing.Optional[datetime.datetime] = None
-        self.entries: typing.List[ElevatorLogEntry] = [entry]
-        self.is_complete: bool = False
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}: {vars(self)}"
-
-
 if __name__ == "__main__":
 
-    input_path = "data/query_result.csv"
-    # input_path = input("Enter source file path...")
-    # if not os.path.exists(input_path) or not os.path.isfile(input_path):
-    #     print(f"file not found or invalid: {input_path}")
-    results: typing.List[ElevatorCall] = []
-    sorted_results = []
-    with open(input_path) as csvfile:
-        reader = csv.DictReader(csvfile)
-        sorted_results = sorted(reader, key=operator.itemgetter("date"), reverse=True)
-        entries = ElevatorLogEntries(
-            [ElevatorLogEntry(**x) for x in sorted_results]
-        ).split_by_date()
-        for k, v in entries.items():
-            # test = ElevatorOperations.from_log_entries(v)
-            # [print(x, "\n") for x in test]
-            print(
-                f"{k}: average {round(ElevatorOperations.from_log_entries(v).average(), 2)} seconds   median: {round(ElevatorOperations.from_log_entries(v).median(), 2)} seconds"
-            )
+    import os
+
+    def get_user_input_file_path() -> str:
+        """get_user_input_file_path prompts user for input file path and returns the path
+
+        Raises:
+            FileNotFoundError: if file is not found
+
+        Returns:
+            str: validated path of file
+        """
+        input_path = input("Enter source file path:  ")
+        if not os.path.exists(input_path) or not os.path.isfile(input_path):
+            print(f"File not found: {input_path}")
+            exit(1)
+        return input_path
+
+    def load_entries_from_csv(input_path: str) -> typing.List[typing.Any]:
+        """load_entries_from_csv loads elevator log entries from a csv file.
+
+        Args:
+            input_path (str): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        with open(input_path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            return sorted(reader, key=operator.itemgetter("date"), reverse=True)
+
+    input_path = get_user_input_file_path()
+
+    entries = ElevatorLogEntries(
+        [ElevatorLogEntry(**x) for x in load_entries_from_csv(input_path)]
+    ).split_by_date()
+    for k, v in entries.items():
+        print(
+            f"{k}: average {round(ElevatorOperations.from_log_entries(v).average(), 2)} seconds   median: {round(ElevatorOperations.from_log_entries(v).median(), 2)} seconds"
+        )
